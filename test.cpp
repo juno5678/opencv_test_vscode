960789d36c3612a82bc61cff1a50cc15fdee4f64
Juno <juno55789@gmail.com>
2022-07-08 13:31:14 +0800

    init

---
+++ test.cpp
@@ -0,0 +1,23 @@
+
+#include "iostream"
+
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+
+    std::cout << "aa" << std::endl;
+
+    std::string img = "carpet.jpg";
+    namedWindow("test");
+    Mat srcImage = imread(img);
+    if (!srcImage.data)
+    {
+        return 1;
+    }
+    imshow("srcImage", srcImage);
+    waitKey(0);
+    return 0;
+}
